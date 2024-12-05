b9b8d65ba668542bd75254042d5418540a55d3f1
Esteban Fernandes <119616775+EstebanFernandes@users.noreply.github.com>
2024-12-05 20:11:34 +0100

    pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { WaitingRoomPageComponent } from './waiting-room-page/waiting-room-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+  { path: 'room/:idRoom', component: WaitingRoomPageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirection par défaut
+  { path: '**', redirectTo: '/home' } // Gestion des routes non trouvées
+];
