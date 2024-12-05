c7b9289dfa2ab250903dfe501dc9e9c2f08a6c34
33768 <louis.demathan@free.fr>
2024-12-05 23:39:28 +0100

    add waiting room

--- src/app/waiting-room-page/waiting-room-page.component.ts
+++ src/app/waiting-room-page/waiting-room-page.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, input } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button'; // import Button Module
+import { FormsModule } from '@angular/forms';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-waiting-room-page',
   standalone: true,
-  imports: [],
+  imports: [CardModule, InputTextModule, ButtonModule, FormsModule, DividerModule],
   templateUrl: './waiting-room-page.component.html',
   styleUrl: './waiting-room-page.component.css'
 })
 export class WaitingRoomPageComponent {
+  idRoom: number = 0
+  playerList: string[]  = []
+  hostId: number = 0
+  playerId: number = 1
 
+  ngOnInit(){
+    this.playerList = ['malo', "antoine"]
+  }
 }
