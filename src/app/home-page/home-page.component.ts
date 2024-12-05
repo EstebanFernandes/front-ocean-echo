e2a338bfe1e40bc99ae2a635184352cf21a03b49
Esteban Fernandes <119616775+EstebanFernandes@users.noreply.github.com>
2024-12-05 21:34:57 +0100

    home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button'; // import Button Module
+import { FormsModule } from '@angular/forms';
+import { DividerModule } from 'primeng/divider';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, ButtonModule, FormsModule, DividerModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  nameHost: string = ""
+  nameJoin: string = ""
+  nameIdJoin: string = ""
+
+
+  createRoom() {
+    if (this.nameHost !== "") {
+      //logic to create room
+    }
+  }
+
+  joinRoom() {
+    if (this.nameJoin !== "" && this.nameIdJoin !== "") {
+      //logic to join room
+    }
+  }
 
 }
